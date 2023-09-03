5d3abc43c6493f3c052b04467db7964cf7c8bd1b
hari <hariprasad12994@gmail.com>
2023-09-03 23:22:09 +0200

    Package state as of 01092023, backup of prev pc config

--- scripts/user_installed_package_lister.sh
+++ scripts/user_installed_package_lister.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pacman -Qqe | grep -v "$(awk '{print $1}' /desktopfs-pkgs.txt)" >\
-../out/user_installed_packages.txt
+# pacman -Qqe | grep -v "$(awk '{print $1}' /desktopfs-pkgs.txt)" >\
+# ../out/user_installed_packages.txt
+pacman -Qqe > ../out/user_installed_packages.txt
